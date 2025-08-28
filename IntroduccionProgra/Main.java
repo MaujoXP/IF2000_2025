5fe7337d93bffe37c1e6b7b79367c9958fe13415
Mau <mauriciobermudez1404@gmail.com>
2025-08-28 01:00:39 -0600

    Finalización Lab01

--- IntroduccionProgra/Main.java
+++ IntroduccionProgra/Main.java
@@ -1,19 +1,34 @@
 package IF2000_2025.IntroduccionProgra;
 //import IF2000_2025.IntroduccionProgra.domain.Person;
 import IF2000_2025.IntroduccionProgra.logic.Ejercicios_Semana2_PracticaEnClase;
+import IF2000_2025.IntroduccionProgra.logic.Laboratorio_1;
 import IF2000_2025.IntroduccionProgra.logic.Quiz3;
 
 public class Main {
   public static void main(String[] args) {
     //Ejercicios_Semana2_PracticaEnClase pruebaEjercicio = new Ejercicios_Semana2_PracticaEnClase();
-    Quiz3 pruebaQuiz3 = new Quiz3();
+    //Quiz3 pruebaQuiz3 = new Quiz3();
+    Laboratorio_1 pruebaLab1 = new Laboratorio_1();
+
     /* pruebaEjercicio.ejercicioA();
     pruebaEjercicio.ejercicioB();
     pruebaEjercicio.ejercicioC();
     pruebaEjercicio.ejercicioD(new int[] {5, 3, 8, 6, 2});
     pruebaEjercicio.ejercicioE();
     pruebaEjercicio.ejercicioF(); */
-    pruebaQuiz3.ejercicioA();
+    //pruebaQuiz3.ejercicioA();
+    //System.out.println(pruebaLab1.ejercicio_1());
+    //System.out.println(pruebaLab1.ejercicio_2());
+    //System.out.println(pruebaLab1.ejercicio_3());
+    //pruebaLab1.ejercicio_4(); 
+    //pruebaLab1.ejercicio_5();
+    //pruebaLab1.ejercicio_6();
+    //pruebaLab1.ejercicio_7();
+    //pruebaLab1.ejercicio_8();
+    //pruebaLab1.ejercicio_9();
+    //pruebaLab1.ejercicio_10();
+    //pruebaLab1.ejercicio_11();
+    pruebaLab1.ejercicio_12();
 
     /* Person p = new Person("John", "Doe", "123456789", "555-1234", 30);
     p.toString();
